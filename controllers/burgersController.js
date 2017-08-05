2a2ac266774aa4b1f3e90680ac068091befad8bd
ais047 <hursungwoo@yahoo.com>
2017-08-05 02:52:26 -0700

    d

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.findAll({}).then(function(data) {
+  burger.sequelize.findAll({}).then(function(data) {
     var hbsObject = {
       burgers: data
     };
